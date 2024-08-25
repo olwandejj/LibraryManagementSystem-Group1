234391b5c035fd432452c5e4376b303e036344a7
Olwande <93462797+olwandejj@users.noreply.github.com>
2024-08-26 00:31:15 +0300

    Update tests.py

--- LibraryManagementSystem/library/tests.py
+++ LibraryManagementSystem/library/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from django.urls import reverse
 from .models import Author, Category, Book, Member, Loan
 from django.contrib.auth.models import User
+from datetime import date
 
 class AuthorAPITests(APITestCase):
 
@@ -81,6 +82,19 @@ class BookAPITests(APITestCase):
         }
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_update_book(self):
+        url = reverse('book-detail', args=[self.book.id])
+        data = {
+            "title": "Harry Potter",
+            "description": "A young boy discovers he is a wizard. Revised edition.",
+            "author": self.author.id,
+            "category": self.category.id,
+            "isbn": "1234567890123",
+            "copies_available": 10
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
     def test_delete_book(self):
@@ -100,17 +114,27 @@ class MemberAPITests(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_create_member(self):
+        new_user = User.objects.create_user(username='newuser', password='12345')
+        data = {"user": new_user.id, "address": "456 Book Blvd"}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+        self.assertEqual(Member.objects.count(), 2)
+        self.assertEqual(Member.objects.get(user=new_user).address, "456 Book Blvd")
 
     def test_update_member(self):
         url = reverse('member-detail', args=[self.member.id])
         data = {"user": self.user.id, "address": "789 Fiction Road"}
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.member.refresh_from_db()
+        self.assertEqual(self.member.address, "789 Fiction Road")
 
     def test_delete_member(self):
         url = reverse('member-detail', args=[self.member.id])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Member.objects.count(), 0)
 
 
 class LoanAPITests(APITestCase):
@@ -120,16 +144,38 @@ class LoanAPITests(APITestCase):
         self.member = Member.objects.create(user=self.user, address="123 Library Lane")
         self.author = Author.objects.create(name="J.K. Rowling", biography="Author of the Harry Potter series.")
         self.category = Category.objects.create(name="Fantasy")
-        self.book = Book.objects.create(title="Harry Potter", description="A young boy discovers he is a wizard.", author=self.author, category=self.category, isbn="1234567890", copies_available=5)
-        self.loan = Loan.objects.create(member=self.member, book=self.book, loan_date="2024-08-25")
+        self.book = Book.objects.create(title="Harry Potter", description="A young boy discovers he is a wizard.", author=self.author, category=self.category, isbn="1234567890123", copies_available=5)
+        self.loan = Loan.objects.create(member=self.member, book=self.book)
         self.url = reverse('loan-list')
 
     def test_get_all_loans(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_create_loan(self):
+        data = {
+            "book": self.book.id,
+            "member": self.member.id,
+            "return_date": "2024-09-26"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Loan.objects.get(id=response.data['id']).return_date, date(2024, 9, 26))
+
+    def test_update_loan(self):
+        url = reverse('loan-detail', args=[self.loan.id])
+        data = {
+            "book": self.loan.book.id,
+            "member": self.loan.member.id,
+            "return_date": "2024-09-01"
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.loan.refresh_from_db()
+        self.assertEqual(self.loan.return_date, date(2024, 9, 1))
 
     def test_delete_loan(self):
         url = reverse('loan-detail', args=[self.loan.id])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Loan.objects.count(), 0)
