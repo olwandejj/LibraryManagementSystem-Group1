4108bbaf315af03f454d7d3c7604d36e5934d829
Godwill Kiprop Kiplagat <93596110+Godwill98@users.noreply.github.com>
2024-08-26 12:19:25 +0300

    Update admin.py

--- LibraryManagementSystem/library/admin.py
+++ LibraryManagementSystem/library/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Author
+from .models import Book
+from .models import Category
+from .models import Member
+from .models import  Loan
+
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Member)
+admin.site.register(Loan)
+admin.site.register(Category)
